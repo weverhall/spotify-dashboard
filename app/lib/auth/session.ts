445c819874a0316f41af5ed966c462bd2de27037
w <weverhall@gmail.com>
2025-12-05 00:42:53 +0200

    Modify session code and try Render's Redis

--- app/lib/auth/session.ts
+++ app/lib/auth/session.ts
@@ -1,11 +1,43 @@
 import crypto from 'crypto';
-import { kv } from '@vercel/kv';
+import { createClient, RedisClientType } from 'redis';
+import { AuthTokenSchema, AuthToken } from '../schemas';
+
+const redisURL = process.env.REDIS_URL!;
+
+declare global {
+  var redisClient: RedisClientType;
+}
+
+let client: RedisClientType;
+
+if (!globalThis.redisClient) {
+  client = createClient({ url: redisURL });
+  client.on('error', (err) => console.error('redis client error', err));
+  await client.connect();
+  globalThis.redisClient = client;
+} else {
+  client = globalThis.redisClient;
+}
 
 export const generateSessionID = () => crypto.randomBytes(32).toString('hex');
 
-export const storeSession = async (sessionID: string, token: unknown) =>
-  await kv.set(`session:${sessionID}`, token, { ex: 3600 });
+export const storeSession = async (sessionID: string, token: AuthToken) => {
+  await client.set(`session:${sessionID}`, JSON.stringify(token), { EX: 3600 });
+};
 
-export const getSession = async (sessionID: string) => await kv.get(`session:${sessionID}`);
+export const getSession = async (sessionID: string): Promise<AuthToken | null> => {
+  const data = await client.get(`session:${sessionID}`);
+  if (!data) return null;
 
-export const deleteSession = async (sessionID: string) => await kv.del(`session:${sessionID}`);
+  try {
+    const parsed: unknown = JSON.parse(data);
+    return AuthTokenSchema.parse(parsed);
+  } catch {
+    console.error('failed to parse session from redis or invalid token');
+    return null;
+  }
+};
+
+export const deleteSession = async (sessionID: string) => {
+  await client.del(`session:${sessionID}`);
+};
