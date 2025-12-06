081e94fe9ad223aaff7b2e4043db36958967e69f
w <weverhall@gmail.com>
2025-12-06 20:17:30 +0200

    Disable function call temporarily to get initial refresh token into Redis

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import GlobalTracks from './components/GlobalTracks';
-import getGlobalTopTracks from './lib/globalTracks';
+// import GlobalTracks from './components/GlobalTracks';
+// import getGlobalTopTracks from './lib/globalTracks';
 
 const Home = async () => {
-  const items = await getGlobalTopTracks();
+  // const items = await getGlobalTopTracks();
 
   return (
     <main>
       <h1>Global Top 50</h1>
-      <GlobalTracks items={items} />
+      {/*<GlobalTracks items={items} />*/}
     </main>
   );
 };
